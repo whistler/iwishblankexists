42dd2dd7792415867f464bea6ff823a45f852618
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-12-01 15:01:57 -0500

    linked wishes and wishnames

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,4 +1,12 @@
 class Wish < ActiveRecord::Base
-  attr_accessible :ip, :location, :wishname_id
+  attr_accessible :ip, :location, :wishname_id, :name
   belongs_to :wishname, :counter_cache => :wish_count
+
+  before_save :find_wishname
+  attr_accessor :name
+
+  def find_wishname
+    self.wishname_id = Wishname.find_or_create_by_name(self.name).id
+  end
+
 end
