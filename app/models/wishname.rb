4c5e25c856b833a2cd08bec1adce361edbac40ff
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-12-01 16:37:33 -0500

    added tag cloud

--- app/models/wishname.rb
+++ app/models/wishname.rb
@@ -1,4 +1,10 @@
 class Wishname < ActiveRecord::Base
   attr_accessible :name
   has_many :wishes
+
+  def cloud_size
+    @max ||= Wishname.maximum("wish_count")
+    @min ||= Wishname.minimum("wish_count")
+    return 1 + (@max - self.wish_count) * (10-1) / (@max - @min)
+  end
 end
