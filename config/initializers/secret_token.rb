ae460d814682a2e6293ac185381b9a06b965eaf8
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-12-01 13:06:12 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iwishblankexists::Application.config.secret_token = '3dbb22bc5c18cf424654c333f8eeb11862123c9c1ba038227f5bd2db6bac373cb88837a454682996948b73a6a85b5b3bb3d1369966d7b2cae822e65ca6fa29c1'
